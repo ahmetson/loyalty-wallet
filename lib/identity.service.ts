b20eb808786e4d1e50e440daa72fb616ca33f27e
Medet Ahmetson <milayter@gmail.com>
2023-11-30 18:50:02 +0700

    Identity (a user + wallet)

---
+++ lib/identity.service.ts
@@ -0,0 +1,39 @@
+import type { W3CCredential } from '@0xpolygonid/js-sdk'
+import { CredentialStatusType, IdentityStorage, core } from '@0xpolygonid/js-sdk'
+import { PolygonIdService } from './polygon-id.service'
+
+const config = useRuntimeConfig()
+
+export class IdentityServices {
+  static instanceIS: {
+    did: core.DID
+    credential: W3CCredential
+  }
+
+  static async createIdentity(seed: Uint8Array) {
+    if (!this.instanceIS) {
+      const polygonService = PolygonIdService.getExtensionServiceInstance()
+
+      const identity = await polygonService.wallet?.createIdentity({
+        method: core.DidMethod.PolygonId,
+        blockchain: core.Blockchain.Polygon,
+        networkId: core.NetworkId.Mumbai,
+        revocationOpts: {
+          type: CredentialStatusType.Iden3ReverseSparseMerkleTreeProof,
+          id: config.public.RHS_URL,
+        },
+        seed,
+      })
+      console.log('!!!!!!!!!!!!!!!!', identity)
+      this.instanceIS = identity!
+      return this.instanceIS
+    }
+    else {
+      return this.instanceIS
+    }
+  }
+
+  static getIdentityInstance() {
+    return this.instanceIS
+  }
+}
