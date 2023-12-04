32e1edcafe5191baac6bdfb918ec914b8a8707e2
Medet Ahmetson <milayter@gmail.com>
2023-12-05 01:26:40 +0700

    remove aborting (used for a debug)

--- middleware/accounts.global.ts
+++ middleware/accounts.global.ts
@@ -5,8 +5,8 @@ export default defineNuxtRouteMiddleware((from, to) => {
   const accounts = useLocalStorage<Account[]>('accounts', [])
   const wallet = useLocalStorage<HDNodeWallet | undefined>('wallet', undefined)
 
-  if (to.path === '/welcome' && wallet.value !== undefined && accounts.value.length > 0)
-    return abortNavigation('account created')
+  // if (to.path === '/welcome' && wallet.value !== undefined && accounts.value.length > 0)
+  //   return abortNavigation('account created')
 
   if (accounts.value.length === 0 && from.path !== '/welcome')
     return navigateTo('/welcome')
